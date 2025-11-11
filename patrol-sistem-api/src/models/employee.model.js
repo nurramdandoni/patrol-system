944bc7a6a3d19762ea31f1970f1dcde82f6cfaaa
Doni Nurramdan <nuramdandoni@gmail.com>
2025-11-11 15:22:08 +0700

    [employee] : update field

--- patrol-sistem-api/src/models/employee.model.js
+++ patrol-sistem-api/src/models/employee.model.js
@@ -2,9 +2,11 @@ module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define('employee', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     user_id: { type: DataTypes.INTEGER, allowNull: true },
+    nip: { type: DataTypes.STRING, allowNull: false },
     full_name: { type: DataTypes.STRING, allowNull: false },
     ttl: { type: DataTypes.DATE, allowNull: false },
     gender: { type: DataTypes.ENUM('male', 'female'), allowNull: false },
+    nik: { type: DataTypes.STRING, allowNull: false },
     address: { type: DataTypes.TEXT, allowNull: false },
     phone: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
