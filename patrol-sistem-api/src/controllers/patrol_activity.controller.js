72427d171fbfffcf25a991311013df011da7738c
Doni Nurramdan <nuramdandoni@gmail.com>
2025-11-12 11:51:13 +0700

    [date] : update filter date in partol activity

--- patrol-sistem-api/src/controllers/patrol_activity.controller.js
+++ patrol-sistem-api/src/controllers/patrol_activity.controller.js
@@ -1,37 +1,43 @@
-const bcrypt = require('bcryptjs');
-const { PatrolActivity, Location, User } = require('../models');
-const jwtUtils = require('../utils/jwt');
-const { json, Op } = require('sequelize');
-const checkPermission = require('../utils/checkPermission');
+const bcrypt = require("bcryptjs");
+const { PatrolActivity, Location, User } = require("../models");
+const jwtUtils = require("../utils/jwt");
+const { json, Op } = require("sequelize");
+const checkPermission = require("../utils/checkPermission");
 
 exports.patrolActivity = async (req, res) => {
   try {
-    const menuId = 4;                       // /admin/location
-    const permissionId = [1];               // 1 view 2 create, 3 edit, 4 delete, 5 print
+    const menuId = 4; // /admin/location
+    const permissionId = [1]; // 1 view 2 create, 3 edit, 4 delete, 5 print
     // validasi akses
     const allowed = await checkPermission(menuId, permissionId, req.user);
     if (!allowed) {
       return res.status(401).json({
-        status: 'Access',
-        message: 'Access Not Allowed',
+        status: "Access",
+        message: "Access Not Allowed",
       });
     }
-    
+
     const page = parseInt(req.query.page) || 1;
-    const rowCount = parseInt(req.query.rowCount) || 10;
+    const rowCount = parseInt(req.query.rowCount) || 100;
     const offset = (page - 1) * rowCount;
 
     const { date_from, date_to } = req.query;
 
     const where = {};
     if (date_from && date_to) {
-      where.createdAt = {
-        [Op.between]: [new Date(date_from), new Date(date_to)],
+      // ubah ke format YYYY-MM-DDT00:00:00 lokal
+      const fromDate = new Date(`${date_from}T00:00:00`);
+      const toDate = new Date(`${date_to}T23:59:59`);
+
+      where.check_date = {
+        [Op.between]: [fromDate, toDate],
       };
     } else if (date_from) {
-      where.createdAt = { [Op.gte]: new Date(date_from) };
+      const fromDate = new Date(`${date_from}T00:00:00`);
+      where.check_date = { [Op.gte]: fromDate };
     } else if (date_to) {
-      where.createdAt = { [Op.lte]: new Date(date_to) };
+      const toDate = new Date(`${date_to}T23:59:59`);
+      where.check_date = { [Op.lte]: toDate };
     }
 
     const { count, rows } = await PatrolActivity.findAndCountAll({
@@ -39,24 +45,22 @@ exports.patrolActivity = async (req, res) => {
       limit: rowCount,
       offset,
       include: [
-        {model:User, attributes:['id','username']},
-        {model:Location, attributes:['id','name']}
-      ]
+        { model: User, attributes: ["id", "username"] },
+        { model: Location, attributes: ["id", "name"] },
+      ],
     });
-
-    
 
     res.json({
       statusCode: 200,
-      status: 'Success',
-      message: 'Data Berhasil Ditemukan!',
+      status: "Success",
+      message: "Data Berhasil Ditemukan!",
       totalData: count,
       data: rows,
     });
   } catch (err) {
     res.status(500).json({
-      status: 'Error',
-      message: 'Terjadi Kesalahan Saat Menampilkan Data Patrol Activity!',
+      status: "Error",
+      message: "Terjadi Kesalahan Saat Menampilkan Data Patrol Activity!",
       data: err.message,
     });
   }
