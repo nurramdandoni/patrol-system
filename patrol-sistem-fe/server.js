24624e6d3d63e399934b8aa963d4cca0365694b4
Doni Nurramdan <nuramdandoni@gmail.com>
2025-11-18 20:27:16 +0700

    [template] : update new design template

--- patrol-sistem-fe/server.js
+++ patrol-sistem-fe/server.js
@@ -1,20 +1,20 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 const app = express();
 
 // ====== Konfigurasi EJS + Layout ======
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
-app.set('layout', 'layouts/main2'); // default layout
+app.set("layout", "layouts/main2"); // default layout
 
 // ====== Middleware ======
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // penting buat form POST
-app.use(express.static(path.join(__dirname, 'public'))); // folder public untuk CSS/JS/img
+app.use(express.static(path.join(__dirname, "public"))); // folder public untuk CSS/JS/img
 
 app.use((req, res, next) => {
   res.locals.API_URL = process.env.API_URL; // variabel global EJS
@@ -22,27 +22,61 @@ app.use((req, res, next) => {
 });
 
 // ====== Routing ======
-app.get('/', (req, res) => {
-  res.render('signin/index', { layout: 'layouts/blanks', title: 'Sign In Page' });
+app.get("/", (req, res) => {
+  res.render("signin/index", {
+    layout: "layouts/blanks",
+    title: "Sign In Page",
+  });
 });
-app.get('/layout', (req, res) => {
-  res.render('dashboard/index', { title: 'Dashboard Page' });
-});
-
-app.get('/location', (req, res) => {
-  res.render('location', { title: 'Security Kinenta' , titlePage: 'Location' });
-});
-
-app.get('/patrol-activity', (req, res) => {
-  res.render('patrol-activity', { title: 'Security Kinenta' , titlePage: 'Patrol Activity' });
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard/index", { title: "Beranda" });
 });
 
-app.get('/patrol-list', (req, res) => {
-  res.render('patrol-list', { title: 'Security Kinenta' , titlePage: 'Patrol List' });
+app.get("/profile", (req, res) => {
+  res.render("profile/index", { title: "Profile" });
 });
 
-app.get('/scanner', (req, res) => {
-  res.render('scanner', { title: 'Scanner' });
+app.get("/location", (req, res) => {
+  res.render("location/index", { title: "Lokasi" });
+});
+
+app.get("/patrol-activity", (req, res) => {
+  res.render("patrol-activity/index", {
+    title: "Aktivitas Patroli",
+  });
+});
+
+const tanggalSekarang = new Date().toLocaleDateString("id-ID", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+app.get("/patrol-list", (req, res) => {
+  res.render("patrol-list/index", {
+    title: "Daftar Patroli",
+    titlePage: "Patrol List",
+    today:tanggalSekarang
+  });
+});
+app.get("/employee", (req, res) => {
+  res.render("employee/index", { title: "Pegawai" });
+});
+app.get("/user", (req, res) => {
+  res.render("user/index", { title: "Pengguna" });
+});
+app.get("/role", (req, res) => {
+  res.render("role/index", { title: "Role" });
+});
+app.get("/menu", (req, res) => {
+  res.render("menu/index", { title: "Menu" });
+});
+app.get("/access", (req, res) => {
+  res.render("access/index", { title: "Akses" });
+});
+
+app.get("/scanner", (req, res) => {
+  res.render("scanner", { layout: "layouts/blanks", title: "Scanner" });
 });
 
 // ====== Jalankan Server ======
