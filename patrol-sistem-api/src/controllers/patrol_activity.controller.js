4a5417a54ea17b8b000702b7b542cd5bffa611eb
Doni Nurramdan <nuramdandoni@gmail.com>
2026-01-09 06:33:26 +0700

    add dashboard and api counting patrol total every date

--- patrol-sistem-api/src/controllers/patrol_activity.controller.js
+++ patrol-sistem-api/src/controllers/patrol_activity.controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const { PatrolActivity, Location, User } = require("../models");
 const jwtUtils = require("../utils/jwt");
-const { json, Op } = require("sequelize");
+const { json, Op, fn, col, literal } = require("sequelize");
 const checkPermission = require("../utils/checkPermission");
 
 exports.patrolActivity = async (req, res) => {
@@ -18,7 +18,7 @@ exports.patrolActivity = async (req, res) => {
     }
 
     const page = parseInt(req.query.page) || 1;
-    const rowCount = parseInt(req.query.rowCount) || 100;
+    const rowCount = parseInt(req.query.rowCount) || 10;
     const offset = (page - 1) * rowCount;
 
     const { date_from, date_to } = req.query;
@@ -65,3 +65,60 @@ exports.patrolActivity = async (req, res) => {
     });
   }
 };
+
+exports.dashboardData = async (req, res) => {
+  try {
+    const menuId = 4; // /admin/location
+    const permissionId = [1]; // 1 view 2 create, 3 edit, 4 delete, 5 print
+    // validasi akses
+    const allowed = await checkPermission(menuId, permissionId, req.user);
+    if (!allowed) {
+      return res.status(401).json({
+        status: "Access",
+        message: "Access Not Allowed",
+      });
+    }
+    const { date_from, date_to } = req.query;
+
+    const where = {};
+    if (date_from && date_to) {
+      // ubah ke format YYYY-MM-DDT00:00:00 lokal
+      const fromDate = new Date(`${date_from}T00:00:00`);
+      const toDate = new Date(`${date_to}T23:59:59`);
+
+      where.check_date = {
+        [Op.between]: [fromDate, toDate],
+      };
+    } else if (date_from) {
+      const fromDate = new Date(`${date_from}T00:00:00`);
+      where.check_date = { [Op.gte]: fromDate };
+    } else if (date_to) {
+      const toDate = new Date(`${date_to}T23:59:59`);
+      where.check_date = { [Op.lte]: toDate };
+    }
+
+    const rows = await PatrolActivity.findAll({
+    where,
+      attributes: [
+        [fn('DATE', col('check_date')), 'date'],
+        [fn('COUNT', col('id')), 'total']
+      ],
+      group: [fn('DATE', col('check_date'))],
+      order: [[fn('DATE', col('check_date')), 'ASC']]
+    });
+
+    res.json({
+      statusCode: 200,
+      status: "Success",
+      message: "Data Berhasil Ditemukan!",
+      data: rows,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Error",
+      message: "Terjadi Kesalahan Saat Menampilkan Data Patrol Activity!",
+      data: err.message,
+    });
+  }
+};
+
--- patrol-sistem-api/src/routes/patrol_activity.routes.js
+++ patrol-sistem-api/src/routes/patrol_activity.routes.js
@@ -4,5 +4,6 @@ const guard = require('../middleware/auth.middleware');
 const patrolActivityController = require('../controllers/patrol_activity.controller');
 
 router.get('/patrol-activity', guard.verifyAuth, patrolActivityController.patrolActivity);
+router.get('/dashboard-data', guard.verifyAuth, patrolActivityController.dashboardData);
 
 module.exports = router;
